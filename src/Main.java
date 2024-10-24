2d69f5a6712e45d5dac8499930616006987bc8a2
Vishal Bhore <mr.vishalbhore@gmail.com>
2024-10-24 22:46:04 +0530

    this demo

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+         Point p1 = new Point();
+         p1.draw();
+
+        System.out.println();
+
+        Point p2 = new Point(100, 100);
+        p2.draw();
+            }
+}
